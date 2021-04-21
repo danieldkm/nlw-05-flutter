c0ad2b81e06fc1532a2e627421c4fc4fbdb41cce
Daniel K. Morita <daniel.morita@sotran.com.br>
2021-04-21 20:57:54 -0300

    aula03

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -5,6 +5,15 @@ import 'package:DevQuiz/shared/widgets/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
+  final String title;
+  final String completed;
+  final double percent;
+  const QuizCardWidget({
+    Key? key,
+    required this.title,
+    required this.completed,
+    required this.percent,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -31,7 +40,7 @@ class QuizCardWidget extends StatelessWidget {
           //   height: 24,
           // ),
           Text(
-            'Gerenciamento de Estado',
+            title,
             style: AppTextStyles.heading15,
           ),
           // SizedBox(
@@ -41,14 +50,14 @@ class QuizCardWidget extends StatelessWidget {
             children: [
               Expanded(
                 child: Text(
-                  '3 de 10',
+                  completed,
                   style: AppTextStyles.body11,
                 ),
               ),
               Expanded(
                 flex: 4,
                 child: ProgressIndicatorWidget(
-                  value: 0.3,
+                  value: percent,
                 ),
               ),
             ],
