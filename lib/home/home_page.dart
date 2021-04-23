4e285c07294f022f52b0a354d64de4de450043e3
Daniel K. Morita <daniel.morita@sotran.com.br>
2021-04-23 20:11:19 -0300

    aula04

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:DevQuiz/challenge/challenge_page.dart';
 import 'package:DevQuiz/core/app_colors.dart';
 import 'package:DevQuiz/home/home_state.dart';
 import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
@@ -39,22 +40,25 @@ class _HomePageState extends State<HomePage> {
               SizedBox(
                 height: 24,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  LevelButtonWidget(
-                    label: 'Fácil',
-                  ),
-                  LevelButtonWidget(
-                    label: 'Médio',
-                  ),
-                  LevelButtonWidget(
-                    label: 'Difícil',
-                  ),
-                  LevelButtonWidget(
-                    label: 'Perito',
-                  ),
-                ],
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    LevelButtonWidget(
+                      label: 'Fácil',
+                    ),
+                    LevelButtonWidget(
+                      label: 'Médio',
+                    ),
+                    LevelButtonWidget(
+                      label: 'Difícil',
+                    ),
+                    LevelButtonWidget(
+                      label: 'Perito',
+                    ),
+                  ],
+                ),
               ),
               SizedBox(
                 height: 24,
@@ -65,12 +69,24 @@ class _HomePageState extends State<HomePage> {
                   mainAxisSpacing: 16,
                   crossAxisCount: 2,
                   children: controller.quizzes!
-                      .map((e) => QuizCardWidget(
-                            title: e.title,
-                            completed:
-                                '${e.questionAnswered}/${e.questions.length}',
-                            percent: e.questionAnswered / e.questions.length,
-                          ))
+                      .map(
+                        (e) => QuizCardWidget(
+                          title: e.title,
+                          completed:
+                              '${e.questionAnswered}/${e.questions.length}',
+                          percent: e.questionAnswered / e.questions.length,
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ChallengePage(
+                                  questions: e.questions,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      )
                       .toList(),
                 ),
               ),
